06fe1316423eeaa376a5a31764ca6282bbd442c4
robinmahto <robinmahto413@gmail.com>
2024-02-22 19:17:12 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import app from "./app";
 import { Config } from "./config/config";
+import logger from "./config/logger";
 
 const startServer = () => {
     try {
         const PORT = Config.PORT;
-        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (error) {
-        console.error(error);
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
