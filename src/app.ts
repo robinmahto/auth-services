d1d495cc9065b67beb0f22caeefb28d134b9a8a0
robinmahto <robinmahto413@gmail.com>
2024-02-23 06:38:37 +0530

    implementing error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.send(JSON.stringify({ message: "Welcome to the Auth services" }));
+});
+
+// middlware for error handling
+app.use((err: HttpError, _req: Request, res: Response, _next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
