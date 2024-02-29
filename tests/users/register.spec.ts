8438525e3d88268ce394307f0026bca1c3326305
robinmahto <robinmahto413@gmail.com>
2024-03-01 04:27:33 +0530

    testing user persistence

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
+import { User } from "../../src/entity/User";
 
 describe("POST /auth/register", () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        // database truncate
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe("Given all fields", () => {
         it("it should return the 201 status code", async () => {
             // Arrange
@@ -35,6 +53,24 @@ describe("POST /auth/register", () => {
             expect(response.headers["content-type"]).toEqual(
                 expect.stringContaining("json"),
             );
+        });
+
+        it("should persist user in the database", async () => {
+            // Arrange
+            const userData = {
+                firstName: "robin",
+                lastName: "mahto",
+                email: "robin@gmail.com",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+            //  Assert
+            const userRepository = connection.getRepository(User);
+            const users = userRepository.find();
+            expect(users).toHaveLength(1);
         });
     });
 
