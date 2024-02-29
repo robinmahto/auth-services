18f4b5eb0979ac36efa8d97fefc03353aaa4ff72
robinmahto <robinmahto413@gmail.com>
2024-03-01 03:47:20 +0530

    database connection with typeORM

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,15 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entity/User";
+import { Config } from "./config";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    // dont use in production
+    synchronize: Config.NODE_ENV === "dev" || Config.NODE_ENV === "test",
     logging: false,
     entities: [User],
     migrations: [],
