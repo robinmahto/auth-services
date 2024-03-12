f5853700a3a2b135c8c57644f8b4eaa96bfc8433
robinmahto <robinmahto413@gmail.com>
2024-03-12 22:10:18 +0530

    password hashing

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,16 +3,20 @@ import { User } from "../entity/User";
 import { UserData } from "../types";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
+import bcrypt from "bcrypt";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
+        // hashed password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         try {
             await this.userRepository.save({
                 firstName,
                 lastName,
                 email,
-                password,
+                password: hashedPassword,
                 role: Roles.CUSTOMER,
             });
         } catch (err) {
