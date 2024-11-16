bae6977a3deaebff8e81414cb3e885ed4f39b99a
robinmahto <robinmahto413@gmail.com>
2024-11-16 18:02:51 +0530

    "Added dotenv dependency, updated package-lock.json, and modified server.ts to import Config"

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+import { Config } from './config';
+
 function welcome(name: string) {
-  console.log('Welcome to the game', name);
+  // console.log('Welcome to the game', name, Config.PORT);
+  return {
+    message: `Welcome to the game ${name}`,
+    port: Config.PORT,
+  };
 }
 
 welcome('jhon');
