4f7b804733692e0dec375434abc698d97c4b794a
robinmahto <robinmahto413@gmail.com>
2024-11-18 21:35:06 +0530

    disabled unused var

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,7 @@ export default tseslint.config(
     },
     rules: {
       'no-console': 'off',
-      '@typescript-eslint/no-unused-vars':"off"
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 );
