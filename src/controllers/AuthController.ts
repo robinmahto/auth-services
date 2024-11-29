e2254760fa23ffced5e8c6d40fa7dd66f4c45269
robinmahto <robinmahto413@gmail.com>
2024-11-29 21:38:23 +0530

    "Added logger to AuthController, updated signup method to handle errors and log requests, and modified UserService to catch and throw errors during user creation"

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,15 +1,36 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { SignupUserRequest } from '../types';
 import { UserService } from '../services/UserService';
+import { Logger } from 'winston';
 
 export class AuthController {
   userService: UserService;
-  constructor(userService: UserService) {
+  constructor(
+    userService: UserService,
+    private logger: Logger,
+  ) {
     this.userService = userService;
   }
-  async signup(req: SignupUserRequest, res: Response) {
+  async signup(req: SignupUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body;
-    await this.userService.create({ firstName, lastName, email, password });
-    res.status(201).json({});
+    this.logger.debug('New request to register a user', {
+      firstName,
+      lastName,
+      email,
+      password: '**********',
+    });
+    try {
+      const user = await this.userService.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      this.logger.info('User has been registered', { id: user });
+      res.status(201).json({ id: user });
+    } catch (error) {
+      next(error);
+      return;
+    }
   }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,12 +3,15 @@ import { AuthController } from '../controllers/AuthController';
 import { UserService } from '../services/UserService';
 import { AppDataSource } from '../config/data-source';
 import { User } from '../entity/User';
+import logger from '../config/logger';
 
 const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService);
+const authController = new AuthController(userService, logger);
 
-router.post('/signup', (req, res) => authController.signup(req, res));
+router.post('/signup', (req, res, next) =>
+  authController.signup(req, res, next),
+);
 
 export default router;
